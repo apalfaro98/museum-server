65110115fa4e94200610eb1e32ba483eef38b4d0
Angelica Perez <apalfaro9803@gmail.com>
2022-10-05 23:47:42 -0400

    REFACTOR: para una visializacion de la estadistica en frontend

--- src/routes/naturales.routes.js
+++ src/routes/naturales.routes.js
@@ -1,29 +1,30 @@
 const router = require('express').Router();
 const naturalesController = require('../controllers/naturales.controllers');
 const naturalesValidations = require('../validations/naturales.validations');
+const validateToken = require('../middlewares/validateToken.middleware');
 const validateFields = require('../middlewares/validateFields.middleware');
 
 router
-	.get('/', naturalesController.getAll)
-	.get(
-		'/:id',
-		[...naturalesValidations.getOne, validateFields],
-		naturalesController.getOne
-	)
-	.post(
-		'/',
-		[...naturalesValidations.create, validateFields],
-		naturalesController.create
-	)
-	.put(
-		'/:id',
-		[...naturalesValidations.update, validateFields],
-		naturalesController.update
-	)
-	.delete(
-		'/:id',
-		[...naturalesValidations.delete, validateFields],
-		naturalesController.delete
-	);
+    .get('/', naturalesController.getAll)
+    .get(
+        '/:id',
+        [...naturalesValidations.getOne, validateFields],
+        naturalesController.getOne
+    )
+    .post(
+        '/',
+        [validateToken, ...naturalesValidations.create, validateFields],
+        naturalesController.create
+    )
+    .put(
+        '/:id',
+        [validateToken, ...naturalesValidations.update, validateFields],
+        naturalesController.update
+    )
+    .delete(
+        '/:id',
+        [validateToken, ...naturalesValidations.delete, validateFields],
+        naturalesController.delete
+    );
 
 module.exports = router;
--- src/routes/prestamos.routes.js
+++ src/routes/prestamos.routes.js
@@ -1,29 +1,30 @@
 const router = require('express').Router();
 const prestamosController = require('../controllers/prestamos.controllers');
 const prestamosValidations = require('../validations/prestamos.validations');
+const validateToken = require('../middlewares/validateToken.middleware');
 const validateFields = require('../middlewares/validateFields.middleware');
 
 router
-	.get('/', prestamosController.getAll)
-	.get(
-		'/:id',
-		[...prestamosValidations.getOne, validateFields],
-		prestamosController.getOne
-	)
-	.post(
-		'/',
-		[...prestamosValidations.create, validateFields],
-		prestamosController.create
-	)
-	.put(
-		'/:id',
-		[...prestamosValidations.update, validateFields],
-		prestamosController.update
-	)
-	.delete(
-		'/:id',
-		[...prestamosValidations.delete, validateFields],
-		prestamosController.delete
-	);
+    .get('/', prestamosController.getAll)
+    .get(
+        '/:id',
+        [...prestamosValidations.getOne, validateFields],
+        prestamosController.getOne
+    )
+    .post(
+        '/',
+        [validateToken, ...prestamosValidations.create, validateFields],
+        prestamosController.create
+    )
+    .put(
+        '/:id',
+        [validateToken, ...prestamosValidations.update, validateFields],
+        prestamosController.update
+    )
+    .delete(
+        '/:id',
+        [validateToken, ...prestamosValidations.delete, validateFields],
+        prestamosController.delete
+    );
 
 module.exports = router;
